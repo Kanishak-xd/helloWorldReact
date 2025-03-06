74b1e306d13549640959f4bcf7a1b85028076aa7
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-03-06 11:42:19 +0530

    added image Profile component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import HelloWorld from "./components/HelloWorld";
 import GreetFunc from "./components/GreetFunc";
 import Welcome from "./components/Welcome";
+import Profile from "./components/Profile";
+import Gallery from "./components/Profile";
+import catImage from "C:\\Users\\Aizaw\\Documents\\Studies\\SEM VI\\Capstone\\prac31\\hello-world-react\\src\\cat.jpg";
+
+function MyOwn() {
+  return <img src={catImage} alt="cat picture" />;
+}
 
 function App() {
   return (
@@ -9,6 +16,9 @@ function App() {
       <HelloWorld />
       <GreetFunc name="Kanishak" />
       <Welcome object="user" />
+      <Profile />
+      <Gallery />
+      <MyOwn />
     </div>
   );
 }
