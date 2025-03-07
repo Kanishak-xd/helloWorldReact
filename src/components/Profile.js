e65f3e1f3cd569faf375e442eedb97a79cac5492
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-03-07 23:34:41 +0530

    katherine changed triple cats 100 width

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,17 @@
 import React from "react";
+import catImage from "C:\\Users\\Aizaw\\Documents\\Studies\\SEM VI\\Capstone\\prac31\\hello-world-react\\src\\cat.jpg";
 
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+function Cat() {
+  return <img src={catImage} width="100" />;
 }
 
 export default function Gallery() {
   return (
     <section>
       <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Cat />
+      <Cat />
+      <Cat />
     </section>
   );
 }
